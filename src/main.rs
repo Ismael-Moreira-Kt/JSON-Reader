f1f55cf211b32c1938aa9d299f57871db1ce8d46
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-14 03:29:59 +0100

    feat: Adds read arguments and file contents

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use serde::{Deserialize, Serialize};
+use std::{env, fs, process};
+
 
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -12,4 +14,21 @@ struct Article {
     article: String,
     author: String,
     paragraph: Vec<Paragraph>,
+}
+
+
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    if args.len() < 2 {
+        println!("Usage: {} <filename>", args[0]);
+        process::exit(0);
+    }
+
+    let file_name = &env::args().nth(1).unwrap();
+    let content = fs::read_to_string(file_name).expect("The file could not be read");
+    
+    let parsed: Article = serde_json::from_str(&content).expect("Error when parsing JSON");
+    println!("{:#?}", parsed);
 }
