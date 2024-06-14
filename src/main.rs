bdf270b22528e37351ea18b8bd2dfb24fd7b82c6
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-06-14 03:28:50 +0100

    feat: Create Paragraph and Article structures

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,15 @@
 use serde::{Deserialize, Serialize};
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Paragraph {
+    name: String,
+}
+
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Article {
+    article: String,
+    author: String,
+    paragraph: Vec<Paragraph>,
+}
